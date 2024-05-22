24cd20352b7c8be40ed0310f78522b516ed0694a
Emilia Haramina <emilia.haramina1@gmail.com>
2024-05-22 09:31:22 +0200

    NetworkManager in Maps

--- DIPRAD_Gaze_Tracking_Study/Assets/Scripts/NetworkManager.cs
+++ DIPRAD_Gaze_Tracking_Study/Assets/Scripts/NetworkManager.cs
@@ -9,58 +9,90 @@ using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    public GameObject connectingToServerText;
-    public GameObject connectingToLobbyText;
-    public GameObject connectingToRoomText;
-    public GameObject waitingForPlayerText;
-    public GameObject gameStartingText;
+    public GameObject[] connectingToServerTexts;
+    public GameObject[] connectingToLobbyTexts;
+    public GameObject[] connectingToRoomTexts;
+    public GameObject[] waitingForPlayerTexts;
+    //public GameObject gameStartingText;
 
-    public GameObject gameMenu;
-    public Button startButton;
+    public GameObject startButton; 
 
-    public GameObject mapText;
-    public GameObject mapDropdown;
-    public GameObject gamemodeText;
-    public GameObject gamemodeDropdown;
-    public GameObject ownerChangingText;
+    //public GameObject mapText;
+    //public GameObject mapDropdown;
+    //public GameObject gamemodeText;
+    //public GameObject gamemodeDropdown;
+    public GameObject ownerStartingText;
 
     public GameObject player;
 
     void Start()
     {
         Debug.Log("Connecting to server...");
-        connectingToServerText.SetActive(true);
+
+        foreach (var connectingToServerText in connectingToServerTexts)
+        {
+            connectingToServerText.SetActive(true);
+        }
+
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to server.");
-        connectingToServerText.SetActive(false);
+
+        foreach (var connectingToServerText in connectingToServerTexts)
+        {
+            connectingToServerText.SetActive(false);
+        }
+
         PhotonNetwork.AutomaticallySyncScene = true;
         Debug.Log("Connecting to lobby...");
-        connectingToLobbyText.SetActive(true);
+
+        foreach (var connectingToLobbyText in connectingToLobbyTexts)
+        {
+            connectingToLobbyText.SetActive(true);
+        }
+
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
         Debug.Log("Connected to lobby.");
-        connectingToLobbyText.SetActive(false);
+
+        foreach (var connectingToLobbyText in connectingToLobbyTexts)
+        {
+            connectingToLobbyText.SetActive(false);
+        }
+
         Debug.Log("Creating or joining new room...");
-        connectingToRoomText.SetActive(true);
+
+        foreach (var connectingToRoomText in connectingToRoomTexts)
+        {
+            connectingToRoomText.SetActive(true);
+        }
+
         PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions() { MaxPlayers = 2 }, null);
     }
 
     public override void OnJoinedRoom()
     {
         Debug.Log("Successfully joined the room.");
-        connectingToRoomText.SetActive(false);
+
+        foreach (var connectingToRoomText in connectingToRoomTexts)
+        {
+            connectingToRoomText.SetActive(false);
+        }
+
         player = PhotonNetwork.Instantiate("NetworkedPrefabs/Player", new Vector3(0, 0, 0), Quaternion.identity, 0);
 
         if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
         {
-            waitingForPlayerText.SetActive(true);
+            foreach (var waitingForPlayerText in waitingForPlayerTexts)
+            {
+                waitingForPlayerText.SetActive(true);
+            }
         }
         else
         {
@@ -70,36 +102,44 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        waitingForPlayerText.SetActive(false);
+        foreach (var waitingForPlayerText in waitingForPlayerTexts)
+        {
+            waitingForPlayerText.SetActive(false);
+        }
+
         ShowGameMenu();
     }
 
     public void ShowGameMenu()
     {
-        gameMenu.SetActive(true);
+        ownerStartingText.SetActive(true);
+        startButton.SetActive(true);
+
         if (!PhotonNetwork.IsMasterClient)
         {
-            startButton.interactable = false;
-            mapText.SetActive(false);
-            mapDropdown.SetActive(false);
-            gamemodeText.SetActive(false);
-            gamemodeDropdown.SetActive(false);
+            startButton.GetComponent<Button>().interactable = false;
+            //mapText.SetActive(false);
+            //mapDropdown.SetActive(false);
+            //gamemodeText.SetActive(false);
+            //gamemodeDropdown.SetActive(false);
         }
         else
         {
-            ownerChangingText.SetActive(false);
+            //ownerStartingText.SetActive(false);
         }
     }
 
     public void StartGame()
     {
         Debug.Log("Game starting...");
-        int mapValue = mapDropdown.GetComponent<TMP_Dropdown>().value;
-        int gamemodeValue = gamemodeDropdown.GetComponent<TMP_Dropdown>().value;
-        gameMenu.SetActive(false);
-        gameStartingText.SetActive(true);
+        //int mapValue = mapDropdown.GetComponent<TMP_Dropdown>().value;
+        //int gamemodeValue = gamemodeDropdown.GetComponent<TMP_Dropdown>().value;
+
+        // 
+
+        //gameStartingText.SetActive(true);
         // TODO: Add to scriptable object
 
-        PhotonNetwork.LoadLevel(mapValue + 1);
+        //PhotonNetwork.LoadLevel(mapValue + 1);
     }
 }
