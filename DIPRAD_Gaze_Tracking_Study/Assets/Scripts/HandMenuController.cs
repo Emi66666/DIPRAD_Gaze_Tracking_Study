a3e9df87d4639f277d8c64e42ae54353057cccc0
NevenPralas <np54410@fer.hr>
2024-05-27 10:23:11 +0200

    HandMenu position update

--- DIPRAD_Gaze_Tracking_Study/Assets/Scripts/HandMenuController.cs
+++ DIPRAD_Gaze_Tracking_Study/Assets/Scripts/HandMenuController.cs
@@ -12,31 +12,31 @@ public class HandMenuController : MonoBehaviour
     public RectTransform handMenu;
     public Transform gazeInteractor;
 
-    private GameObject leftControllerPrefab;
-    private GameObject rightControllerPrefab;
+    private SkinnedMeshRenderer leftHand;
+    private SkinnedMeshRenderer rightHand;
 
     void Start()
     {
         OVRCameraRig rig = FindAnyObjectByType<OVRCameraRig>();
-        leftControllerPrefab = rig.transform.Find("OVRInteractionComprehensive/OVRControllers/LeftController/OVRControllerVisual/OVRControllerPrefab").gameObject;
-        rightControllerPrefab = rig.transform.Find("OVRInteractionComprehensive/OVRControllers/RightController/OVRControllerVisual/OVRControllerPrefab").gameObject;
+        leftHand = rig.transform.Find("OVRInteractionComprehensive/OVRHands/LeftHandGrabUseSynthetic/OVRLeftHandVisual/OculusHand_L/l_handMeshNode").GetComponent<SkinnedMeshRenderer>();
+        rightHand = rig.transform.Find("OVRInteractionComprehensive/OVRHands/RightHandGrabUseSynthetic/OVRRightHandVisual/OculusHand_R/r_handMeshNode").GetComponent<SkinnedMeshRenderer>();
     }
 
     void Update()
     {
-        if (leftControllerPrefab.activeSelf || rightControllerPrefab.activeSelf)
-        {
-            handMenu.position = controllerPoint.position;
-            handMenu.rotation = controllerPoint.rotation;
-            gazeInteractor.position = gazeInteractorControllerPoint.position;
-            gazeInteractor.rotation = gazeInteractorControllerPoint.rotation;
-        }
-        else
+        if (leftHand.enabled || rightHand.enabled)
         {
             handMenu.position = handPoint.position;
             handMenu.rotation = handPoint.rotation;
             gazeInteractor.position = gazeInteractorHandPoint.position;
             gazeInteractor.rotation = gazeInteractorHandPoint.rotation;
+        }
+        else
+        {
+            handMenu.position = controllerPoint.position;
+            handMenu.rotation = controllerPoint.rotation;
+            gazeInteractor.position = gazeInteractorControllerPoint.position;
+            gazeInteractor.rotation = gazeInteractorControllerPoint.rotation;
         }
     }
 }
